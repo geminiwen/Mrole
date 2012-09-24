f9e70dd2a04de286067b138206aa928b5eff571e
Zhao Qian <453255811@qq.com>
2012-09-24 18:48:54 +0800

    update sql&Mrole_API.doc

--- mrole.sql
+++ mrole.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- 主机: localhost
--- 生成日期: 2012 年 09 月 23 日 14:52
+-- 生成日期: 2012 年 09 月 24 日 10:46
 -- 服务器版本: 5.1.36
 -- PHP 版本: 5.3.0
 
@@ -38,6 +38,7 @@ CREATE TABLE IF NOT EXISTS `album_info` (
 --
 
 INSERT INTO `album_info` (`album_name`, `album_user`, `build_time`, `update_time`) VALUES
+('HeadPortrait', '1000304217', '2012-09-24 17:15:14', '2012-09-24 17:15:14'),
 ('zhaoqian', '1000304217', '2012-09-22 18:39:42', '2012-09-22 18:44:59');
 
 -- --------------------------------------------------------
@@ -52,16 +53,25 @@ CREATE TABLE IF NOT EXISTS `friend` (
   `friend_id` varchar(10) NOT NULL COMMENT '好友学号',
   `type` int(2) NOT NULL COMMENT '是好友或者关注者',
   PRIMARY KEY (`ID`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=20 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=29 ;
 
 --
 -- 转存表中的数据 `friend`
 --
 
 INSERT INTO `friend` (`ID`, `user_id`, `friend_id`, `type`) VALUES
+(27, '1000304217', '1000304217', 0),
+(26, '1000304217', '1000304217', 0),
+(25, '1000304217', '1000304217', 0),
+(24, '1000304217', '1000304217', 0),
+(23, '1000304217', '1000304217', 0),
+(22, '1000304217', '1000304217', 0),
+(21, '0900301214', '0900301214', 0),
+(20, '1000304217', '1000304217', 0),
 (19, '1000304217', '1000304217', 0),
 (17, '1000304217', '0900301214', 1),
-(18, '0900301214', '0900301214', 0);
+(18, '0900301214', '0900301214', 0),
+(28, '1000304217', '1000304217', 0);
 
 -- --------------------------------------------------------
 
@@ -143,6 +153,7 @@ CREATE TABLE IF NOT EXISTS `status` (
   `status_content` varchar(250) NOT NULL COMMENT '内容',
   `status_user` varchar(10) NOT NULL COMMENT '学号',
   `status_time` datetime NOT NULL COMMENT '发表时间',
+  `status_commet_num` int(10) NOT NULL COMMENT '状态评论条数',
   PRIMARY KEY (`status_id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
 
@@ -150,9 +161,9 @@ CREATE TABLE IF NOT EXISTS `status` (
 -- 转存表中的数据 `status`
 --
 
-INSERT INTO `status` (`status_id`, `status_content`, `status_user`, `status_time`) VALUES
-(1, 'zhaoqian', '1000304217', '2012-09-23 10:11:00'),
-(2, 'hello', '0900301214', '2012-09-23 10:13:09');
+INSERT INTO `status` (`status_id`, `status_content`, `status_user`, `status_time`, `status_commet_num`) VALUES
+(1, 'zhaoqian', '1000304217', '2012-09-23 10:11:00', 0),
+(2, 'hello', '0900301214', '2012-09-23 10:13:09', 0);
 
 -- --------------------------------------------------------
 
@@ -208,5 +219,5 @@ CREATE TABLE IF NOT EXISTS `stu_info` (
 --
 
 INSERT INTO `stu_info` (`stu_username`, `stu_realname`, `stu_password`, `stu_job`, `stu_birthday`, `stu_email`, `stu_sex`, `stu_sex2`, `stu_constellation`, `stu_school`, `stu_college`, `stu_class`, `stu_tel`, `stu_question`, `stu_answer`, `stu_checked`) VALUES
-('0900301214', '温盛章', '948b5904da610204ec4edff8a069bf3b', '会长', '1992-06-07', '123@qq.com', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, b'1'),
-('1000304217', '赵倩', 'cc9f4328e026878631d31720239402d3', '团支书', '1991-12-07', '453255811@qq.com', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, b'0');
+('0900301214', '温盛章', '14e1b600b1fd579f47433b88e8d85291', '学生会', '1992-09-06', '1234@qq.com', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, b'1'),
+('1000304217', '赵倩', 'ec6a6536ca304edf844d1d248a4f08dc', '团支书', '1991-12-07', '453255811@qq.com', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, b'1');
